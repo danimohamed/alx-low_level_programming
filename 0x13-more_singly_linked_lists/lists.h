09d38f93b9c9db1253f75c303b42747a4474d3ee
danimohamed <mohameddani993@gmail.com>
2023-05-02 15:18:18 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,11 +1,7 @@
-#ifndef LIST_T_H
-#define LIST_T_H
+#ifndef LISTS_H
+#define LISTS_H
 
-#include <stdio.h>
 #include <stddef.h>
-#include <string.h>
-#include <stdlib.h>
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -20,8 +16,6 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* prototypes */
-
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -33,5 +27,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
+int _putchar(char c);
 
-#endif /* LIST_T_H */
+#endif /*LISTS_H*/
