47e43153106048b8ec2798885f42c996813b32fa
danimohamed <mohameddani993@gmail.com>
2024-01-18 00:57:44 +0100

    search algo

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -15,9 +15,9 @@
  */
 typedef struct listint_s
 {
-	int n;
-	size_t index;
-	struct listint_s *next;
+    int n;
+    size_t index;
+    struct listint_s *next;
 } listint_t;
 
 /**
@@ -32,10 +32,10 @@ typedef struct listint_s
  */
 typedef struct skiplist_s
 {
-	int n;
-	size_t index;
-	struct skiplist_s *next;
-	struct skiplist_s *express;
+    int n;
+    size_t index;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
 } skiplist_t;
 
 int linear_search(int *array, size_t size, int value);
@@ -47,4 +47,4 @@ int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
 
-#endif
+#endif /* _SORTING_ALGOS_H_ */
